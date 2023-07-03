2b3e587621ccdf85cc2f60c463a4df076d9352a2
Sergey Maximov <95900933+SergeyMaxx@users.noreply.github.com>
2023-07-03 14:02:22 +0300

    15. Firebase и трансформеры данных

--- src/app/services/http.service.js
+++ src/app/services/http.service.js
@@ -1,10 +1,31 @@
 import axios from 'axios'
 import {toast} from 'react-toastify'
-import config from '../config.json'
+import configFile from '../config.json'
 
-axios.defaults.baseURL = config.apiEndpoint
+axios.defaults.baseURL = configFile.apiEndpoint
 
-axios.interceptors.response.use(res => res,
+axios.interceptors.request.use(config => {
+    if (configFile.isFireBase) {
+      const containSlash = /\/$/gi.test(config.url)
+      config.url = (containSlash ? config.url.slice(0, -1) : config.url) + '.json'
+    }
+    return config
+  },
+  function (error) {
+    return Promise.reject(error)
+  }
+)
+
+const transformData = data => data
+  ? Object.keys(data).map(key => ({...data[key]}))
+  : []
+
+axios.interceptors.response.use(res => {
+    if (configFile.isFireBase) {
+      res.data = {content: transformData(res.data)}
+    }
+    return res
+  },
   function (error) {
     const expectedErrors =
       error.response &&
